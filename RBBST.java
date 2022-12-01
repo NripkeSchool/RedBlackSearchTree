79fe58312f5ed93d89fc0bb5914463bb540b4b4b
NripkeSchool <89931379+NripkeSchool@users.noreply.github.com>
2022-12-01 15:29:55 -0500

    Update RBBST.java with get()

--- RBBST.java
+++ RBBST.java
@@ -2,6 +2,7 @@ public class RBBST<Key extends Comparable<Key>, Value>
 {
   public static final boolean RED = true;
   public static final boolean BLACK = false;
+  private Node root;
   
   private boolean isRed(Node n)
   {
@@ -155,6 +156,22 @@ public class RBBST<Key extends Comparable<Key>, Value>
     }
     
     return balance(top);
+  }
+  
+  public Value get(Key k)
+  {
+      Node n = get(root, k);
+      if (n == null) {return null;}
+      return n.val;
+  }
+    
+  private Node get(Node parent, Key k)
+  {
+      if (parent == null) {return null;}
+      if (parent.k == k) {return parent;}
+      if (k.compareTo(parent.k) > 0) {return get(parent.right, k);}
+        
+      return get(parent.left, k);
   }
   
   private Node balance(Node top)
